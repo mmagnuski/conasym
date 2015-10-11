e87b951eb573b1919c6b295350ca613fd5b16b45
mmagnuski <mmagnuski@swps.edu.pl>
2015-10-11 19:27:19 +0200

    some fixes - this code may be responsible for strange effects that Tomek is getting

--- mass_get_freq_asym.m
+++ mass_get_freq_asym.m
@@ -11,18 +11,19 @@ for r = 1:db_len
 end
 
 lens = cellfun(@length, all_asym);
-if unique(lens) > 1
+if length(unique(lens)) > 1
     error(['Files are of different lengths. ',...
         'This is not implemented. Contact Miko ',...
         'for further assistance.']);
 end
-all_asym = [all_asym{:}];
-fnms = {db.filepath};
+all_len = unique(lens);
+all_asym = reshape([all_asym{:}], [db_len, all_len]);
+fnms = {db.filename};
 
 % out data format:
 table = cell(size(all_asym) + 1);
-table(2:end,1) = fnms;
+table(2:end,1) = fnms';
 table(1, 2:end) = num2cell(out.time);
-table(2:end, 2:end) = all_asym;
+table(2:end, 2:end) = num2cell(all_asym);
 
 xlswrite(fullfile(opt.savepath, 'asym.xls'), table);
